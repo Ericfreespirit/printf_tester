c9c29d8496f291fd428597440f55df279bc97088
Eric LING <eric_ling@hotmail.fr>
2020-12-11 09:11:42 +0100

    commit

--- test.c
+++ test.c
@@ -1,9 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-char	*ft_strjoin(char const *s1, char const *s2);
+
+char	*simple_conv()
+{
+	char *ptr;
+
+	if (!(ptr = malloc(sizeof(*ptr)* 5)))
+	return ("Malloc problem");
+	ptr = "eric";
+
+	//1 cspdiuxX
+	printf("1) cspdiuxX\n");
+
+	printf("printf:%c\n",'o');
+	
+	printf("printf:%s\n","Est-ce que ça marche ?");
+	
+	printf("printf:%p\n",ptr);
+	
+	printf("printf:%d\n",42);
+	
+	printf("printf:%i\n",42);
+	
+	printf("printf:%u\n",-2);
+	
+	printf("printf:%x\n",42);
+	
+	printf("printf:%X\n",42);
+
+	//2 special case
+	printf("2) special case\n");
+	//3 multiple arg and conv
+	printf("3) multiple arg and conv\n");
+	//4 flags everywhere
+	printf("4) flags everywhere\n");
+
+	return("simple_conv test\n");
+}
+
+
+
 int main()
 {
-
-	printf("%s\n",ft_strjoin("123","456"));	
+	printf("%s",simple_conv());
+	printf("--------------------\n");
 	return (0);
 }
