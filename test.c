9f86b76835c56f5869cfaf0e96619bdc27a30618
Eric LING <eric_ling@hotmail.fr>
2020-12-16 19:38:07 +0100

    update main for test multiple case in the purpose to do the handle_param function

--- test.c
+++ test.c
@@ -3,29 +3,101 @@
 
 int	ft_printf(const char *s, ...);
 int	is_valid(char c);
-
+int	n = 42;
+char c = 'A';
+char str[5] = "eric";
+unsigned int u_n = -1;
 //list all output for parsing
 void	parsing_flag()
 {
-	int	i;
-	i = 14;
+	printf("==== width ====\n");
+	printf("%5d\n",n);
+	printf("%5i\n",n);
+	printf("%5c\n",c);
+	printf("%5s\n",str);
+	printf("%5p\n",str);
+	printf("%5u\n",u_n);
+	printf("%5x\n",n);
+	printf("%5X\n",n);
 
-	printf("==== Start to the End of format ====\n");
-	//printf("%d]%d%d%d",i++);
+	printf("==== width | '0' ====\n");
+	printf("%05d\n",n);
+	printf("%05i\n",n);
+	printf("%05c\n",c);
+	printf("%05s\n",str);
+	printf("%05p\n",str);
+	printf("%05u\n",u_n);
+	printf("%05x\n",n);
+	printf("%05X\n",n);
 
+	printf("==== width | '-' ====\n");
+	printf("%-5d\n",n);
+	printf("%-5i\n",n);
+	printf("%-5c\n",c);
+	printf("%-5s\n",str);
+	printf("%-5p\n",str);
+	printf("%-5u\n",u_n);
+	printf("%-5x\n",n);
+	printf("%-5X\n",n);
+	
+	printf("==== width | '0' ====\n");
+	printf("%05d\n",n);
+	printf("%05i\n",n);
+	printf("%05c\n",c);
+	printf("%05s\n",str);
+	printf("%05p\n",str);
+	printf("%05u\n",u_n);
+	printf("%05x\n",n);
+	printf("%05X\n",n);
+
+	printf("==== width | '-' | '0' ====\n");
+	printf("%-05d\n",n);
+	printf("%-05i\n",n);
+	printf("%-05c\n",c);
+	printf("%-05s\n",str);
+	printf("%-05p\n",str);
+	printf("%-05u\n",u_n);
+	printf("%-05x\n",n);
+	printf("%-05X\n",n);
 }
 
-// TODO when passing done make norminette
+void	parsing_precision()
+{
+	printf("==== width | '.'  ====\n");
+	printf("%05.d\n",n);
+	printf("%05.i\n",n);
+	printf("%05.c\n",c);
+	printf("%05.s\n",str);
+	printf("%05.p\n",str);
+	printf("%05.u\n",u_n);
+	printf("%05.x\n",n);
+	printf("%05.X\n",n);
+	
+	printf("==== width | '*'  ====\n");
+	printf("%05*d\n",12,n);
+	printf("%05*i\n",12,n);
+	printf("%05*c\n",12,c);
+	printf("%05*s\n",12,str);
+	printf("%05*p\n",12,str);
+	printf("%05*u\n",12,u_n);
+	printf("%05*x\n",12,n);
+	printf("%05*X\n",12,n);
+	
+	printf("==== width | '.' |'*'  ====\n");
+	printf("%05.*d\n",12,n);
+	printf("%05.*i\n",12,n);
+	printf("%05.*c\n",12,c);
+	printf("%05.*s\n",12,str);
+	printf("%05.*p\n",12,str);
+	printf("%05.*u\n",12,u_n);
+	printf("%05.*x\n",12,n);
+	printf("%05.*X\n",12,n);
+}
+
 int main()
 {	
-	//parsing_flag();
-
-	ft_printf("hello\n");
-	ft_printf("%chello\n");
-	ft_printf("%10c%s%d%i%u%p%x%Xhello\n");
-	ft_printf("%10c%s%-01m0.d%i%u%p%x%Xhello\n");
-	ft_printf("%mhello\n");
-	ft_printf("1%");
-
+	parsing_flag();
+	printf("/////////////////////////////////////\n");
+	parsing_precision();
 		return (0);
 }
