0408ed4553b63133105029cd7b11e1d2d5285317
Eric LING <eriling@e1r9p10.42.fr>
2021-01-08 12:20:32 +0100

    le lutin est parti

--- test.c
+++ test.c
@@ -23,19 +23,24 @@ int main()
 	//printf("%-8d\n",1234567);
 	//printf("%-0-0-000-i\n",1234567);
 
+	// == TEST: precision default
+	
+	//√ printf("%.4d\n",1234);
+	//√ printf("%.5d\n",1234);
+	//printf("%1.5d\n",1234);
 
-	//printf("%-012.*d",10,123456789);
+	ft_printf("%-6.10d\n",1234);
+	
+	//printf("%6.5d\n",1234);
+	//printf("%-6.5d\n",1234);
+	
+	//ft_printf("%.10d\n",1234);
+	
 
-	// == TEST: if precision enable 'o' with width == 
-	//printf("%12.*d",10,123456789);
-	//printf("%012.10d",123456789);
-	//printf("%011.10d",123456789);
-	//printf("%011.d",123456789);
+
 
 	// == TEST: if precision is neg or only '.' ==
 	//printf("%-10.3d\n",42);
-
-	// == TEST: if precision with '0' is ignored
 
 	// == TEST: if precision with '-'
 	//printf("%-4.3d",1);
@@ -45,7 +50,6 @@ int main()
 	//ft_printf("%03d",42);
 	//ft_printf("%-3d",42);
 
-	ft_printf("%-5d",1234);
 
 	return (0);
 }
